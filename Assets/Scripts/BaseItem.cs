d8abe5876b8d4530840454f2ec4227a2d4d239f1
franciaj <Jordan.Francia@viacom.com>
2019-01-26 14:38:36 -0500

    Added base item class

---
+++ Assets/Scripts/BaseItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseItem : MonoBehaviour {
+
+	protected void OnCollisionEnter2D (Collision2D coll) {
+		if (coll.gameObject.CompareTag ("Player")) {
+			Activate(coll.gameObject.GetComponent<Player>());
+		}
+	}
+
+	protected abstract void Activate(Player player);
+}
