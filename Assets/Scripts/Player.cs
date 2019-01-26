d5df678fa3685a2676711711a843d721b7d22dc1
franciaj <Jordan.Francia@viacom.com>
2019-01-26 12:59:03 -0500

    Created player prefab with movement, health, and speed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[Header("Stats")]
+	public int health = 3;
+	public float speed;
+
+	[Header("Controls")]
+	public KeyCode walkRight;
+	public KeyCode walkLeft;
+	public KeyCode walkUp;
+	public KeyCode walkDown;
+
+	void FixedUpdate () {
+		Move ();
+	}
+
+	void Move () {
+		float vert = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		float hori = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		transform.Translate(hori, vert, 0f);
+	}
+}
