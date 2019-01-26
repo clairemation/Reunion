4bc9a522d70ca72e7109ae83af79e043a59cd9a3
Claire Samuels <claire.samuels@viacom.com>
2019-01-26 18:32:38 -0500

    Homing when near

--- Assets/HomingWhenNearMovement.cs
+++ Assets/HomingWhenNearMovement.cs
@@ -4,7 +4,35 @@ using UnityEngine;
 
 public class HomingWhenNearMovement : Movement {
 
-	[SerializeField] CircleCollider2D homingCollider;
+	[SerializeField] float distanceThreshold = 0.5f;
 
+	SlowHomingMovement homingMovement;
+	SineMovement baseMovement;
+	bool isHoming = false;
+	Player player;
 
+	void Awake(){
+		player = FindObjectOfType<Player> ();
+		homingMovement = GetComponent<SlowHomingMovement> ();
+		baseMovement = GetComponent<SineMovement> ();
+	}
+
+	void Update(){
+		if (isHoming) {
+			if (Vector3.Distance (transform.position, player.transform.position) > distanceThreshold) {
+				EnableHoming (false);
+			}
+		} else {
+			if (Vector3.Distance (transform.position, player.transform.position) < distanceThreshold) {
+				EnableHoming (true);
+			}
+		}
+	}
+	
+
+	void EnableHoming(bool enabled){
+		baseMovement.enabled = !enabled;
+		homingMovement.enabled = enabled;
+		isHoming = enabled;
+	}
 }
