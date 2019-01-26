d37c66a6ade9eca790df1455fa38c677ba03cfdb
Christopher Graf <cgraf062491@gmail.com>
2019-01-26 15:48:05 -0500

    SpawnManager and EnemySpawn completed
    Completed the scripts for the spawners to spawn the enemy that is given to them from the SpawnManager.
    
    The SpawnManager chooses a random enemy and a random spawner, then calls that spawner to Instantiate the chosen enemy at it's position

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void SpawnEnemy(GameObject enemy)
+	{
+		Instantiate(enemy, transform.position, Quaternion.identity);
 	}
 }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] spawners;
+
+	[SerializeField]
+	private GameObject[] enemies;
+
+	private GameObject current_spawner;
+
+	private float min_time = 15.0f;
+	private float max_time = 30.0f;
+	private int spawner_num;
+	private int enemy_num;
+	private float spawn_time;
+
+	private bool spawn_ready = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawn_time = Random.Range(min_time, max_time);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		spawn_time -= Time.deltaTime;
+
+		if(spawn_time <= 0 && spawn_ready == false)
+		{
+			spawn_ready = true;
+
+			spawner_num = Random.Range(0, 8);
+			enemy_num = Random.Range(0, 3);
+
+			current_spawner = spawners[spawner_num];
+			current_spawner.GetComponent<EnemySpawn>().SpawnEnemy(enemies[enemy_num]);
+
+			spawn_time = Random.Range(min_time, max_time);
+
+			spawn_ready = false;
+		}
+	}
+}
