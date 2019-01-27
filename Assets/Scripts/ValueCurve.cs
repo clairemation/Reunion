6dd01d9efbe44db6f25634f93ac826aa2ff6a65c
franciaj <Jordan.Francia@viacom.com>
2019-01-27 09:57:20 -0500

    Added a very important space

--- Assets/Scripts/ValueCurve.cs
+++ Assets/Scripts/ValueCurve.cs
@@ -1,1 +1,45 @@
-﻿using System; using System.Collections; using System.Collections.Generic; using UnityEngine;  [Serializable] public class ValueCurve {  	[Serializable] 	public enum CurveType { 		Linear 	}  	[SerializeField] float startVal; 	[SerializeField] float finishVal; 	[SerializeField] CurveType curveType = CurveType.Linear;  	public float GetValue (float x) { 		float result = -1f; 		switch(curveType){ 			case CurveType.Linear: 				result = Lerp(startVal, finishVal, x); 				break; 			default: 				break; 		}  		return result; 	}  	public int GetIntValue (float x) { 		float result = GetValue(x); 		return Mathf.RoundToInt(result); 	}  	float Lerp (float start, float finish, float t) { 		bool reversed = (finish < start); 		return reversed ? ReverseLerp(startVal, finishVal, t) : Mathf.Lerp(startVal, finishVal, t); 	}  	float ReverseLerp(float start, float finish, float t){ 		return start - ((start - finish) * t); 	} }  
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ValueCurve {
+
+	[Serializable]
+	public enum CurveType {
+		Linear
+	}
+
+	[SerializeField] float startVal; 
+	[SerializeField] float finishVal;
+	[SerializeField] CurveType curveType = CurveType.Linear;
+
+	public float GetValue (float x) {
+		float result = -1f;
+		switch(curveType){
+			case CurveType.Linear:
+				result = Lerp(startVal, finishVal, x);
+				break;
+			default:
+				break;
+		}
+
+		return result;
+	}
+
+	public int GetIntValue (float x) {
+		float result = GetValue(x);
+		return Mathf.RoundToInt(result);
+	}
+
+	float Lerp (float start, float finish, float t) {
+		bool reversed = (finish < start);
+		return reversed ? ReverseLerp(startVal, finishVal, t) : Mathf.Lerp(startVal, finishVal, t);
+	}
+
+	float ReverseLerp(float start, float finish, float t){
+		return start - ((start - finish) * t);
+	}
+}
+
