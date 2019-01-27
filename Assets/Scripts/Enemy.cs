1ceed29d279f51ee861df32921af6169e3547035
franciaj <Jordan.Francia@viacom.com>
2019-01-27 12:42:23 -0500

    imported VO SFX, added walking VO to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	[SerializeField] AudioClip walkingSound;
+	[SerializeField] AudioClip hitSound;
+	[SerializeField] AudioClip spawnSound;
+
+	AudioSource audioSource;
+
 	/*
 	if the Enemy collides with a Player it will call the
 	Damage() method on the Player
 	*/
+
+	void Awake(){
+		audioSource = GetComponent<AudioSource>();
+
+		audioSource.clip = walkingSound;
+		audioSource.loop = true;
+		audioSource.Play();
+	}
+
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if(other.gameObject.CompareTag("Player"))
