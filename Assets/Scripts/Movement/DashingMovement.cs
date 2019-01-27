980c471017501457babbaba09db567ef1e646cf9
Claire Samuels <claire.samuels@viacom.com>
2019-01-27 11:17:10 -0500

    Aim and dash enemy

---
+++ Assets/Scripts/Movement/DashingMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashingMovement : BaseMovement {
+
+	[SerializeField] float speed = 0.02f;
+	[SerializeField] float angle = 25f;
+	Vector3 direction;
+
+	Player player;
+
+	void Awake(){
+		player = FindObjectOfType<Player> ();
+	}
+
+	void OnEnable(){
+		Init();
+	}
+
+	void Init(){
+		direction = player.transform.position - transform.position;
+		direction.Normalize ();
+	}
+
+	void FixedUpdate () {
+		transform.position += direction * speed;
+	}
+}
--- Assets/Scripts/Movement/WanderMovement.cs
+++ Assets/Scripts/Movement/WanderMovement.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BouncingMovement : Movement {
+public class WanderMovement : BaseMovement {
 
 	[SerializeField] float speed = 0.05f;
 	[SerializeField] float angle = 25f;
 
 	Vector3 direction;
 	Collider2D collider;
-		
+
+
 	void Awake(){
 		collider = GetComponent<Collider2D> ();
 	}
@@ -25,6 +26,16 @@ public class BouncingMovement : Movement {
 
 	void FixedUpdate () {
 		transform.position += direction * speed;
+	}
+
+	public void SetRandomDirection(){
+		direction = RandomDirection ();
+	}
+
+	Vector3 RandomDirection(){
+		Vector3 direction = Quaternion.Euler (0f, 0f, Random.Range (0f, 360f)) * Vector3.right;
+		direction.Normalize ();
+		return direction;
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
